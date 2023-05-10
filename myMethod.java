5f9818044caccfac862fe9744356c777cbb004ad
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-05-10 23:45:45 +0530

    Add files via upload

---
+++ myMethod.java
@@ -0,0 +1,20 @@
+package javaInterviewProgram;
+
+public class myMethod 
+{
+	static void myMethod() {
+	    System.out.println("I just got executed!");
+	  }
+	static void vishalrai(int a)
+	{
+		System.out.println(a);
+	}
+	
+	  public static void main(String[] args) {
+	    myMethod();
+	    vishalrai(106);
+	    
+	    
+	  }
+
+}
