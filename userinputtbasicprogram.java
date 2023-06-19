c96eea357e02e7d9d987fc4a722dc00efc414844
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-06-19 23:55:53 +0530

    Add files via upload

---
+++ userinputtbasicprogram.java
@@ -0,0 +1,21 @@
+package target200qQQQTTOOnnSS;
+
+import java.util.Scanner;
+
+public class userinputtbasicprogram 
+{
+	public static void main(String []args)
+	{
+	System.out.println("Enter the number");
+	Scanner sc=new Scanner(System.in);
+
+	int num=sc.nextInt();
+	{
+		for(int i=0; i<=num; i++)
+		{
+			System.out.println(num);
+		}
+	}
+	
+	}
+}
