f60111b20f4f55ba5f8bb29194e79ffe3715a2da
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-11-29 18:40:47 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,18 @@
+package pattern.Java.Program;
+
+public class basic {
+	public static void main(String []args)
+	{
+		int num=5;
+		for(int i=1; i<num; i++)
+		{
+			for(int j=1; j<i+1; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
---
+++ patternOne.java
@@ -0,0 +1,19 @@
+package pattern.Java.Program;
+
+public class patternOne 
+{
+	public static void main(String []args)
+	{
+		int num=5;
+		for(int i=1; i<num; i++) {
+			for(int j=1; j<i+1; j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+			
+			
+	}
+
+}
