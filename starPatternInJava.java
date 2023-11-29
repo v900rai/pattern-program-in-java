aeafd21684a3cca459a23d9b38c945b1dddba0c3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-11-29 18:46:39 +0530

    Add files via upload

---
+++ starPatternInJava.java
@@ -0,0 +1,17 @@
+package pattern.Java.Program;
+
+public class starPatternInJava 
+{
+	public static void main(String args[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
