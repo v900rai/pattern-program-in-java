4ff4b88076d3f50f4028c0eee39d6e9a2bccc574
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-11-29 21:57:05 +0530

    Add files via upload

---
+++ patternReversInJava.java
@@ -0,0 +1,17 @@
+package pattern.Java.Program;
+
+public class patternReversInJava 
+{
+	public static void main(String []args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
